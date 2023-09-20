a33a4b527bc6f52d84ccb2b7ffb9aa87b4fa5ee0
himanshisaxena <saxena.himanshi11@gmail.com>
2023-09-20 16:28:50 +0200

    install uuid

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/todo-list-react",
-//   build: {
-//     chunkSizeWarningLimit: 1600
-// }
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/todo-list-react'
-});
+  plugins: [react()],
+  base: "/todo-list-react",
+  build: {
+    chunkSizeWarningLimit: 1600
+}
+})
+
