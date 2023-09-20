9d0e6387cc54ed9852198193365c89c570b738d4
himanshisaxena <saxena.himanshi11@gmail.com>
2023-09-20 15:34:22 +0200

    inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/todo-list-react",
+//   build: {
+//     chunkSizeWarningLimit: 1600
+// }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()],
-  base: "/todo-list-react",
-  build: {
-    chunkSizeWarningLimit: 1600
-}
-})
+    plugins: [react()],
+    base: '/todo-list-react'
+});
